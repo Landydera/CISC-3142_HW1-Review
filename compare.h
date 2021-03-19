f63840250160e4ed7b835f2c621a98262107db1c
Islande Derazin <49322866+Landydera@users.noreply.github.com>
2021-03-19 18:33:00 -0400

    Create compare.h

---
+++ compare.h
@@ -0,0 +1,8 @@
+#include<iostream>
+using namespace std;
+
+// This file declare the three function
+int whichIsBigger(int x, int y, bool &same);
+
+int makeBothSame(int x, int &y);
+void makeFirstBigger(int &x, int &y, bool &swapped);
